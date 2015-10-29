b5bd236f7c12f5a318ce14be678dbb158e16a15c
Matthew <1MatthewHarris1@gmail.com>
2015-10-28 20:05:00 -0700

    Transferring my collatz conjecture assignment for CptS260

---
+++ IntroToCompArch/HWCollatz.s
@@ -0,0 +1,68 @@
+syscall for I/O
+#la
+
+.data
+uInput:
+	.ascii "Please input a positive integer: /000"
+
+errorOutput:
+	.ascii "Your input caused an error/000"
+
+
+odd:
+	mult	$s0, $t2
+	mflo	$s0
+	addi	$s0, $s0, 1
+	bne	$HI, $0, error
+		
+	j	beta
+
+main:
+	addi	$t0, $0, 1	#$t0 = 1
+	addi	$t1, $0, 2	#$t1 = 2
+	addi	$t2, $0, 3	#$t2 = 3
+	
+	addi	$v0, $0, 4
+	syscall
+
+	addi	$v0, $0, 5
+	syscall
+	addi	$s0, $a0, 0
+
+	blt	$s0, $0, error
+
+alpha:
+	div	$s0, $t1	#divide uInput by 2
+	bne	$HI, $0, odd	#if(($s0 % 2) != 0) go to label odd
+	mflo	$s0
+
+beta:
+	addi	$v0, $0, 1
+	addi	$a0, $s0, 0
+	syscall
+	bne	$s0, $t0, alpha	#if $s0 != 1 go to label alpha
+	
+	
+error:
+	addi	$v0, $0, 4
+	syscall
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
